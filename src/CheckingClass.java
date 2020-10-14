33c21823062ab0c8b6ce9f9fe7c4dc4e936464b9
Tilaktiwari <tiwari5tilak@gmail.com>
2020-10-14 19:28:09 -0400

    Hi from Tilak

---
+++ src/CheckingClass.java
@@ -0,0 +1,5 @@
+public class CheckingClass {
+    public static void main(String[] args) {
+        System.out.println("This only for letting you know my cloning is finish thanks for sharing");
+    }
+}
