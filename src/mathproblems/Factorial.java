911b59ac20f5a08f64789974ebf29b18b8deb855
Aldrin <aldrinmalakar@gmail.com>
2020-10-14 20:44:13 -0400

    Test Commit

--- src/mathproblems/Factorial.java
+++ src/mathproblems/Factorial.java
@@ -9,6 +9,17 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        findFactoral();
 
     }
+
+    private static void findFactoral() {
+        int factoral = 1;
+        int number = 5;//It is the number to calculate factorial
+        for (int i = 1; i <= number; i++) {
+            factoral = factoral * i;
+        }
+        System.out.println("Factorial of " + number + " is: " + factoral);
+    }
 }
+
--- src/mathproblems/MakePyramid.java
+++ src/mathproblems/MakePyramid.java
@@ -14,6 +14,28 @@ public class MakePyramid {
 
         */
 
+//Calling to method to perform required code:
+    patternMethod();
 
         }
-  }
+
+    private static void patternMethod() {
+         int rowCount = 6, k = 0;
+
+            for(int i = 1; i <= rowCount; ++i, k = 0) {
+                for(int space = 1; space <= rowCount - i; ++space) {
+                    System.out.print("  ");
+                }
+
+                while(k != 2 * i - 1) {
+                    System.out.print("* ");
+                    ++k;
+                }
+
+                System.out.println();
+            }
+        }
+    }
+
+
+
