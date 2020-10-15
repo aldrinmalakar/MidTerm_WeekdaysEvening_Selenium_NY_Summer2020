774dfbee64ef79a8b011f76aef01cedd261e71de
Aldrin <aldrinmalakar@gmail.com>
2020-10-15 14:58:38 -0400

    uploading backup

--- src/problemandsolutions/ArraySort.java
+++ src/problemandsolutions/ArraySort.java
@@ -1,7 +1,19 @@
 package problemandsolutions;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ArraySort {
     // By using the bubble sort algorithm, write a Java program to sort an integer array of 10 elements in ascending.
+    public static void main(String[] args) {
+
+        ArraySort.bubbleSort();
+    }
+
+    private static void bubbleSort() {
+
+
+    }
 
 
 }
--- src/stringproblems/DuplicateWord.java
+++ src/stringproblems/DuplicateWord.java
@@ -11,9 +11,41 @@ public class DuplicateWord {
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
+        int count;
+        // Converting the sentence into lower case
+        st = st.toLowerCase();
+
+        //Splitting the string into words using built-in function
+        String[] words = st.split(" ");
+
+        //Using for nested loops to determine the
+        System.out.println("The duplicate words are: ");
+        for (int i = 0; i < words.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    count++;
+                    words[j] = "0";
+                }
+            }
+            if (count > 1 && words[i] != "0")
+                System.out.println(words[i]);
+        }
+        //To find average length
+        String[] average = st.split(" ");
+
+        int count2 = 0;
+        int sum = 0;
+        int currentWordLength;
+        String tempString;
+        for (int i = 0; i < average.length; i++) {
+            average[i] = average[i].replaceAll("[^a-zA-Z]", "").toLowerCase();
+            tempString = average[i];
+            currentWordLength = tempString.length();
+            sum += currentWordLength;
+            count2++;
+        }
+        double averageWordLength = sum / count2;
+        System.out.println("Average length of word is: " + averageWordLength);
     }
-
-
-
-
 }
