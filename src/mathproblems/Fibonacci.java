85b0f6ecbab489db85e20ce29b8fe41d80505f70
Aldrin <aldrinmalakar@gmail.com>
2020-10-15 00:17:12 -0400

    Raat 12 tar pore

--- src/mathproblems/Fibonacci.java
+++ src/mathproblems/Fibonacci.java
@@ -1,5 +1,7 @@
 package mathproblems;
 
+import java.util.Scanner;
+
 public class Fibonacci {
     public static void main(String[] args) {
          /*
@@ -7,6 +9,26 @@ public class Fibonacci {
           0,1,1,2,3,5,8,13
          */
 
+    fibonacciSequenceMethod();
+        }
 
+
+    public static void fibonacciSequenceMethod() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("How many Fibonnaci numbers do you want to see?");
+        int n = userInput.nextInt();
+
+        int first = 0;
+        int second =1;
+        int fiboSeq;
+        System.out.println("Your desired length of Fibonacci sequence is: [ " + n +" ]");
+        System.out.print(" the series is: " + first +" "+ second);
+    for (int i = 3; i < n; i++){
+        fiboSeq = first + second;
+        System.out.print(" "+fiboSeq);
+        first = second;
+        second = fiboSeq;
     }
+
+     }
 }
